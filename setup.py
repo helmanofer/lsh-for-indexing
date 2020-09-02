aa81e42c1f506d1f25a3923d4c32476d92b5bd07
Ofer Helman <helmanofer@gmail.com>
2020-09-02 16:36:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lsh-for-indexing",
-    version="0.0.3",
+    version="0.0.4",
     author="Ofer Helman",
     author_email="helmanofer@gmail.com",
     description="Package for indexing vectors to solr/es",
